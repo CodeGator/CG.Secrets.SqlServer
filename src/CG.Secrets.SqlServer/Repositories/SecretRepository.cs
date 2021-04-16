94f646aafb118578e4802895090ab6f95cf83f94
martycook <martinc@valornet.com>
2021-04-16 16:47:40 -0500

    I added data protection for the secret values

--- src/CG.Secrets.SqlServer/Repositories/SecretRepository.cs
+++ src/CG.Secrets.SqlServer/Repositories/SecretRepository.cs
@@ -5,11 +5,14 @@ using CG.Secrets.Models;
 using CG.Secrets.Repositories;
 using CG.Secrets.SqlServer.Repositories.Options;
 using CG.Validations;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Options;
 using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Secret = CG.Secrets.Models.Secret;
 
 namespace CG.Secrets.SqlServer.Repositories
 {
@@ -22,6 +25,19 @@ namespace CG.Secrets.SqlServer.Repositories
         ISecretRepository
     {
         // *******************************************************************
+        // Properties.
+        // *******************************************************************
+
+        #region Properties
+
+        /// <summary>
+        /// This property contains a reference to a data protection provider.
+        /// </summary>
+        protected IDataProtectionProvider DataProtectionProvider { get; }
+
+        #endregion
+
+        // *******************************************************************
         // Constructors.
         // *******************************************************************
 
@@ -33,10 +49,15 @@ namespace CG.Secrets.SqlServer.Repositories
         /// </summary>
         public SecretRepository(
             IOptions<SecretRepositoryOptions> options,
-            DbContextFactory<SecretDbContext> factory
+            DbContextFactory<SecretDbContext> factory,
+            IDataProtectionProvider dataProtectionProvider
             ) : base(options, factory)
         {
-            
+            // Validate the parameters before attempting to use them.
+            Guard.Instance().ThrowIfNull(dataProtectionProvider, nameof(dataProtectionProvider));
+
+            // Save the references.
+            DataProtectionProvider = dataProtectionProvider;
         }
 
         #endregion
@@ -64,6 +85,16 @@ namespace CG.Secrets.SqlServer.Repositories
                 // Defer to the data-context.
                 var model = context.Secrets.FirstOrDefault(
                     x => x.Name == name
+                    );
+
+                // Create a data protector.
+                var dataProtector = DataProtectionProvider.CreateProtector(
+                    nameof(CG.Secrets)
+                    );
+
+                // Unprotect the value of the secret.
+                model.Value = dataProtector.Unprotect(
+                    model.Value
                     );
 
                 // Return the results.
@@ -98,11 +129,21 @@ namespace CG.Secrets.SqlServer.Repositories
                 // Create a context.
                 var context = Factory.Create();
 
+                // Create a data protector.
+                var dataProtector = DataProtectionProvider.CreateProtector(
+                    nameof(CG.Secrets)
+                    );
+
+                // Protect the value of the secret.
+                var protectedValue = dataProtector.Unprotect(
+                    value
+                    );
+
                 // Create a model.
                 var model = new Secret()
                 {
                      Key = name,
-                     Value = value 
+                     Value = protectedValue
                 };
 
                 // Defer to the data-context.
